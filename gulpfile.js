dee93526b7c2971bae7e2225960d3f26240ae6b0
mjt01 <michael.taranto@gmail.com>
2014-05-13 21:43:02 +1000

    Convert to gulp

---
+++ gulpfile.js
@@ -0,0 +1,121 @@
+//--------------------------------
+//  MODULES
+//--------------------------------
+var gulp = require('gulp'),
+    jshint = require('gulp-jshint'),
+    jshintStylish = require('jshint-stylish'),
+    connect = require('gulp-connect'),
+    opn = require('opn'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify'),
+    header = require('gulp-header'),
+    wrap = require('gulp-wrap'),
+    concat = require('gulp-concat'),
+    ngmin = require('gulp-ngmin'),
+    karma = require('gulp-karma'),
+    pkg = require('./package.json'),
+
+
+//--------------------------------
+//  HELPERS
+//--------------------------------
+    karmaConfig = function(action) {
+        return {
+            frameworks: ['jasmine'],
+            browsers: [ 'PhantomJS' ],
+            reporters: [ 'progress', 'coverage' ],
+            preprocessors: {
+                'src/*.js': ['coverage']
+            },
+            coverageReporter: {
+                type : 'html',
+                dir : 'test/coverage/'
+            },
+            action: action
+        };
+    },
+    TEST_FILES = 'test/**/*.spec.js',
+    SRC_FILES = 'src/*.js',
+    KARMA_FILES = [
+        'demo/vendor/angular.min.js',
+        'test/vendor/angular-mocks.js',
+        SRC_FILES,
+        TEST_FILES
+    ],
+    PORT = 9999;
+
+
+//--------------------------------
+//  TASKS
+//--------------------------------
+
+gulp.task('lint', function() {
+    return gulp.src([
+           'demo/scripts/directives.js',
+           SRC_FILES,
+           TEST_FILES
+        ])
+        .pipe(jshint())
+        .pipe(jshint.reporter(jshintStylish));
+});
+
+
+gulp.task('test', function() {
+    return gulp.src(KARMA_FILES)
+        .pipe(karma(karmaConfig('run')))
+        .on('error', function(err) {
+            throw err;
+        });
+});
+
+
+gulp.task('connect', function() {
+    connect.server({
+        root: 'demo',
+        port: PORT,
+        livereload: true
+    });
+});
+
+
+gulp.task('server', ['connect'], function() {
+    opn("http://localhost:" + PORT);
+});
+
+
+gulp.task('reload', function() {
+    return gulp.src('demo/**/*.*')
+        .pipe(connect.reload());
+});
+
+
+gulp.task('build', function () {
+    return gulp.src(SRC_FILES)
+        .pipe(concat('featureFlags.min.js'))
+        .pipe(ngmin())
+        .pipe(uglify())
+        .pipe(wrap('(function(){\n<%= contents %>\n}());'))
+        .pipe(header([
+            '/*!',
+            ' * <%= title %> v<%= version %>',
+            ' *',
+            ' * Copyright <%= new Date().getFullYear() %>, <%= author.name %>',
+            ' */\n\n'
+          ].join('\n'), pkg))
+        .pipe(gulp.dest('demo/scripts'));
+});
+
+
+gulp.task('dev', ['build','server'], function() {
+    gulp.watch(['demo/**/*.*'], ['reload']);
+    gulp.watch(['demo/scripts/*.js', TEST_FILES], ['lint']);
+    gulp.watch(SRC_FILES, ['lint', 'build']);
+    gulp.src(KARMA_FILES)
+        .pipe(karma(karmaConfig('watch')));
+});
+
+
+gulp.task('precommit', ['lint','test','build']);
+
+
+gulp.task('demo', ['build', 'server']);
