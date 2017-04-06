7395961015127133370e33615dff7b0d001024c3
Oana Umbres <oana.umbres@visma.com>
2017-04-06 16:10:44 +0300

    Fixed lint issues

--- test/featureFlagsConfig.provider.spec.js
+++ test/featureFlagsConfig.provider.spec.js
@@ -1,10 +1,10 @@
-(function (angular) {
+(function(angular) {
   'use strict';
 
   var module = angular.mock.module,
     inject = angular.mock.inject;
 
-  describe('Provider: featureFlags', function () {
+  describe('Provider: featureFlags', function() {
     var appName = 'myapp',
       featureFlagConfig,
       flags = [{
@@ -15,48 +15,49 @@
         key: 'FLAG_KEY_2'
       }];
 
-    describe('When no flags are set in the config phase', function () {
-      beforeEach(module('feature-flags', function (featureFlagConfigProvider) {
+    describe('When no flags are set in the config phase', function() {
+      beforeEach(module('feature-flags', function(featureFlagConfigProvider) {
         featureFlagConfigProvider.setInitialFlags(null);
         featureFlagConfigProvider.setAppName(null);
       }));
 
-      beforeEach(inject(function (_featureFlagConfig_) {
+      beforeEach(inject(function(_featureFlagConfig_) {
         featureFlagConfig = _featureFlagConfig_;
       }));
 
-      it('should return an empty array for current feature flags', function () {
+      it('should return an empty array for current feature flags', function() {
         expect(featureFlagConfig).not.toBeUndefined();
         expect(featureFlagConfig.getInitialFlags()).toEqual([]);
         expect(featureFlagConfig.getAppName()).toEqual('');
       });
     });
 
-    describe('When flags are set in the config phase', function () {
-      beforeEach(module('feature-flags', function (featureFlagConfigProvider) {
+    describe('When flags are set in the config phase', function() {
+      beforeEach(module('feature-flags', function(featureFlagConfigProvider) {
         featureFlagConfigProvider.setInitialFlags(flags);
         featureFlagConfigProvider.setAppName(appName);
       }));
 
-      beforeEach(inject(function (_featureFlagConfig_) {
+      beforeEach(inject(function(_featureFlagConfig_) {
         featureFlagConfig = _featureFlagConfig_;
       }));
 
-      it('should init the flags with the ones set in the config phase', function () {
+      it('should init the flags with the ones set in the config phase', function() {
         expect(featureFlagConfig).not.toBeUndefined();
         expect(featureFlagConfig.getInitialFlags()).toEqual(flags);
         expect(featureFlagConfig.getAppName()).toEqual(appName);
       });
     });
 
-    describe('When flags are set in the run phase', function () {
-      beforeEach(module('feature-flags', function (featureFlagConfigProvider) {
+    describe('When flags are set in the run phase', function() {
+      beforeEach(module('feature-flags', function(featureFlagConfigProvider) {
+        featureFlagConfigProvider.setInitialFlags(null);
       }));
-      beforeEach(inject(function (_featureFlagConfig_) {
+      beforeEach(inject(function(_featureFlagConfig_) {
         featureFlagConfig = _featureFlagConfig_;
       }));
 
-      it('should init the flags with the ones set in the run phase', function () {
+      it('should init the flags with the ones set in the run phase', function() {
         expect(featureFlagConfig).not.toBeUndefined();
         featureFlagConfig.setInitialFlags(flags);
         featureFlagConfig.setAppName(appName);
@@ -64,6 +65,5 @@
         expect(featureFlagConfig.getAppName()).toEqual(appName);
       });
     });
-
   });
 }(window.angular));
