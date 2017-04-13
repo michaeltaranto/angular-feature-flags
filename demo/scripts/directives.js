e514e24debb3e63c95b0bcd021dfb33f96e25ea9
JeremyCarlsten <jeremy@jeremycarlsten.com>
2017-04-13 19:38:17 +0100

    Fixing linting errors

--- demo/scripts/directives.js
+++ demo/scripts/directives.js
@@ -32,24 +32,22 @@ angular.module('my-app')
     };
   })
   .directive('code', function(featureFlags) {
-      return {
-        restrict: 'E',
-        scope: {},
-        template: '<div class="panel"><button ng-click="codeCtrl.alert()">Click Me!</button></div>',
-        replace: true,
-        controllerAs: 'codeCtrl',
-        controller: function(){
-          var self = this;
-
-          self.alert = function(){
-            if(featureFlags.isOn('code')) {
-              alert("Hello!");
-            }
-          };
-
-        }
-      };
-    })
+    return {
+      restrict: 'E',
+      scope: {},
+      template: '<div class="panel"><button ng-click="codeCtrl.alert()">Click Me!</button></div>',
+      replace: true,
+      controllerAs: 'codeCtrl',
+      controller: function() {
+        var self = this;
+        self.alert = function() {
+          if (featureFlags.isOn('code')) {
+            console.log('Hello!');
+          }
+        };
+      }
+    };
+  })
   .run(function(featureFlags, $http) {
     featureFlags.set($http.get('../data/flags.json'));
   });
